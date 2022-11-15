94cd57fe3579d9c64c584870c2f878b91976f68b
ChrisClark303 <38655477+ChrisClark303@users.noreply.github.com>
2022-11-15 22:37:27 +0000

    Swagger now works on release

--- LeedsBeerQuest/LeedsBeerQuest.Api/Program.cs
+++ LeedsBeerQuest/LeedsBeerQuest.Api/Program.cs
@@ -16,11 +16,11 @@ builder.Services.AddConfig(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
